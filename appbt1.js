0398d6638558a31405f919da0c96870fd7c0e4a4
nguyenthanhchung090302 <110025385+nguyenthanhchung090302@users.noreply.github.com>
2022-07-28 17:10:03 +0700

    add file homework

---
+++ appbt1.js
@@ -0,0 +1,48 @@
+// bài 1 trong trường hợp nào khi bạn đặt tên biến thì sẽ bị thông báo SyntaxError ? Cho 3 ví dụ về việc đặt tên biến sai trong Javascript?
+
+// let name =("lavie");
+// let sjob =("lavie");
+// let sfor =("lavie");
+
+
+// bài 2 khai báo biến
+
+// let smg = ("rikkei is awesome");
+// console.log(smg);
+
+
+
+// bài 3
+// let  studentCount = 0;
+// console.log(0);
+
+// let msg =("Coding is hard but super cool and fun");
+// console.log(msg);
+
+// let studentCount = 0;
+// studentCount = 30;
+// console.log(studentCount);
+
+// let studentCount = 2002;
+// console.log(2002);
+
+//bài 4
+// alert("tôi tên chung");
+
+//bài 5
+// let a = prompt("bạn tên gì vậy");
+// let b = prompt("tôi tên yêu hãy gọi tôi là anh yêu");
+
+// bài 6 
+// let a = prompt("first name");
+// let b = prompt("last name");
+// alert(a +"hi nguyễn thành chung"+ b);
+
+let b = prompt("nhap canh hinh vuong");
+let dt = (b*b);
+alert("dien tich hinh vuong la "+dt);
+
+
+
+
+
